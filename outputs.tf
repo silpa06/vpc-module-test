697cac5d2be05437db187a414805453e8163777e
Silpa <silpa.jr@gmail.com>
2024-09-27 21:10:25 +0800

    vpc-module-test

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,18 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids 
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids 
+}
+
+output "database_subnet_ids" {
+  value = module.vpc.database_subnet_ids 
+}
+
 # output "az_info" {
 #   value = module.vpc.az_info
 # }
